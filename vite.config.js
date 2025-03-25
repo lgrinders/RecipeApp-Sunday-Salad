cdf9075cd6c60a43cf2fa745666ce8a7cba76c4c
Landon Grinderslev <125698291+lgrinders@users.noreply.github.com>
2025-03-25 14:55:08 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,8 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/RecipeApp-Sunday-Salad/",
   build: {
-<<<<<<< HEAD
     outDir: "dist",
-=======
-    outDir: "dist", // Ensures Vercel picks up the correct output folder
->>>>>>> 4eed7e595f8fe8d88364d1f3cc9c43a75d0d9076
   },
 });
