4eed7e595f8fe8d88364d1f3cc9c43a75d0d9076
Landon Grinderslev <125698291+lgrinders@users.noreply.github.com>
2025-03-25 14:46:57 -0500

    changes for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/RecipeApp-Sunday-Salad/",
+  base: "/",
+  build: {
+    outDir: "dist", // Ensures Vercel picks up the correct output folder
+  },
 });
